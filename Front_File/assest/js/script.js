829b64903bf5640346c97f851eb789d04b3348d3
unknown <hniakbari22@gmail.com>
2024-10-12 12:00:54 +0330

    Add Some File

---
+++ Front_File/assest/js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const password = document.getElementById('inputPass')
+    const eye = document.getElementById('eye')
+    eye.addEventListener('click', function(){
+        if (password.type == 'password') {
+            password.type = 'text'
+            eye.classList.remove("bi-eye-fill");
+            eye.classList.add("bi-eye-slash-fill");
+        } else {
+            password.type = 'password'
+            eye.classList.remove("bi-eye-slash-fill");
+            eye.classList.add("bi-eye-fill");
+        }
+    })
+})
